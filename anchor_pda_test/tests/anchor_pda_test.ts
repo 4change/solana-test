e14a2dfbbc6fed1a5de8cbb9c178a727883ad350
gongxf <gongxf@yunphant.com>
2025-09-16 17:43:22 +0800

    完善 README

--- anchor_pda_test/tests/anchor_pda_test.ts
+++ anchor_pda_test/tests/anchor_pda_test.ts
@@ -16,6 +16,8 @@ describe("pda-account", () => {
     program.programId
   );
 
+  // 请注意，在此示例中，如果您多次调用 initialize 指令，并使用相同的 user 地址作为种子，则交易会失败。
+  // 这是因为在推导出的地址上已经存在一个账户。
   it("Is initialized!", async () => {
     const transactionSignature = await program.methods
       .initialize()
