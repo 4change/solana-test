9b4264fa0df1d6a788d801bb162aad2d509df327
gongxf <gongxf@yunphant.com>
2025-09-19 09:28:47 +0800

    anchor_pda_crud

---
+++ anchor_pda_crud/tests/anchor_pda_crud.ts
@@ -0,0 +1,89 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { AnchorPdaCrud } from "../target/types/anchor_pda_crud";
+import { PublicKey } from "@solana/web3.js";
+
+describe("pda", () => {
+  // const program = pg.program;
+  // const wallet = pg.wallet;
+
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.AnchorPdaCrud as Program<AnchorPdaCrud>;
+  const wallet = provider.wallet as anchor.Wallet;
+
+  const [messagePda, messageBump] = PublicKey.findProgramAddressSync(
+    [Buffer.from("message"), wallet.publicKey.toBuffer()],
+    program.programId
+  );
+
+  const [vaultPda, vaultBump] = PublicKey.findProgramAddressSync(
+    [Buffer.from("vault"), wallet.publicKey.toBuffer()],
+    program.programId
+  );
+
+  it("Create Message Account", async () => {
+    const message = "Hello, World!";
+    const transactionSignature = await program.methods
+      .create(message)
+      .accounts({
+        messageAccount: messagePda,
+      })
+      .rpc({ commitment: "confirmed" });
+
+    const messageAccount = await program.account.messageAccount.fetch(
+      messagePda,
+      "confirmed"
+    );
+
+    console.log(JSON.stringify(messageAccount, null, 2));
+    console.log(
+      "Transaction Signature:",
+      `https://solana.fm/tx/${transactionSignature}?cluster=devnet-solana`
+    );
+  });
+
+  it("Update Message Account", async () => {
+    const message = "Hello, Solana!";
+    const transactionSignature = await program.methods
+      .update(message)
+      .accounts({
+        messageAccount: messagePda,
+        vaultAccount: vaultPda,
+      })
+      .rpc({ commitment: "confirmed" });
+
+    const messageAccount = await program.account.messageAccount.fetch(
+      messagePda,
+      "confirmed"
+    );
+
+    console.log(JSON.stringify(messageAccount, null, 2));
+    console.log(
+      "Transaction Signature:",
+      `https://solana.fm/tx/${transactionSignature}?cluster=devnet-solana`
+    );
+  });
+
+  it("Delete Message Account", async () => {
+    const transactionSignature = await program.methods
+      .delete()
+      .accounts({
+        messageAccount: messagePda,
+        vaultAccount: vaultPda,
+      })
+      .rpc({ commitment: "confirmed" });
+
+    const messageAccount = await program.account.messageAccount.fetchNullable(
+      messagePda,
+      "confirmed"
+    );
+
+    console.log("Expect Null:", JSON.stringify(messageAccount, null, 2));
+    console.log(
+      "Transaction Signature:",
+      `https://solana.fm/tx/${transactionSignature}?cluster=devnet-solana`
+    );
+  });
+});
